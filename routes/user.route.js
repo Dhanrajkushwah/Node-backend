9944d951e859ba9a145830d70b6760a9662f4de3
Dhanraj Kushwah <dhanraj.kushwah007@gmail.com>
2024-07-31 12:02:26 +0530

    Data

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,7 @@
 const userController = require("../controllers/user.controller")
 const express = require("express");
 const router = express.Router();
-
+///Dev to vercel
 router.route('/list')
     .get(userController.list)
 router.route('/create')
