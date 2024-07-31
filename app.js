2224276b68ca1a355e4fc72ddf3df30b88e0fbc5
Dhanraj Kushwah <dhanraj.kushwah007@gmail.com>
2024-07-31 17:20:44 +0530

    Add

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const router = require('./routes');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
 app.use(cors());
 app.use('/api', router);
 
-
-mongoose.connect('mongodb://localhost:27017/CONTACTUS_SERVICE', {
+// Replace the connection string with your MongoDB Atlas connection string
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    serverSelectionTimeoutMS: 30000, // Set a higher timeout value (e.g., 30 seconds) 
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Set a higher timeout value (e.g., 30 seconds)
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 
-
-
+module.exports = app;
